66fd2432c015553ea385a047a9377281a6db82e6
Jon Bosley <jon.b@owletcare.com>
2018-07-31 16:50:21 -0600

    Added software for testing communication protocol and modified communication protocol document

---
+++ software/ibis_data_receiver/ibis_data_receiver/MainForm.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace ibis_data_receiver
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenCloseButton = new System.Windows.Forms.Button();
+            this.serialPortList = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.OutputFileTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // OpenCloseButton
+            // 
+            this.OpenCloseButton.Location = new System.Drawing.Point(342, 42);
+            this.OpenCloseButton.Name = "OpenCloseButton";
+            this.OpenCloseButton.Size = new System.Drawing.Size(167, 43);
+            this.OpenCloseButton.TabIndex = 0;
+            this.OpenCloseButton.Text = "Open";
+            this.OpenCloseButton.UseVisualStyleBackColor = true;
+            this.OpenCloseButton.Click += new System.EventHandler(this.OpenCloseButton_Click);
+            // 
+            // serialPortList
+            // 
+            this.serialPortList.FormattingEnabled = true;
+            this.serialPortList.Location = new System.Drawing.Point(125, 50);
+            this.serialPortList.Name = "serialPortList";
+            this.serialPortList.Size = new System.Drawing.Size(197, 28);
+            this.serialPortList.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Serial port:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 194);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Packets received:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(162, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "None";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Output file:";
+            // 
+            // OutputFileTextBox
+            // 
+            this.OutputFileTextBox.Location = new System.Drawing.Point(125, 126);
+            this.OutputFileTextBox.Name = "OutputFileTextBox";
+            this.OutputFileTextBox.Size = new System.Drawing.Size(309, 26);
+            this.OutputFileTextBox.TabIndex = 6;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 250);
+            this.Controls.Add(this.OutputFileTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.serialPortList);
+            this.Controls.Add(this.OpenCloseButton);
+            this.Name = "MainForm";
+            this.Text = "Ibis Data Receiver";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenCloseButton;
+        private System.Windows.Forms.ComboBox serialPortList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox OutputFileTextBox;
+    }
+}
+
---
+++ software/ibis_data_receiver/ibis_data_receiver/MainForm.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace ibis_data_receiver
+{
+    public partial class MainForm : Form
+    {
+        const byte START_BYTE = 0xA5;
+        const byte PROTOCOL_VERSION_0 = 0x00;
+        const byte MAX_PACKET_PAYLOAD_LEN = 255;
+
+        enum ReceivedCommStates
+        {
+            Idle,
+            ReceiveLength,
+            ReceiveCommand,
+            ReceiveData,
+            ReceiveChecksum
+        };
+
+        private struct ReceivedPacket
+        {
+            public byte protocol;
+            public byte length;
+            public byte command;
+            public byte[] payload;
+        };
+
+        private struct LowSpeedSampleDataHeader
+        {
+            UInt32 timestampMs;
+            UInt16 timeBetweenSamplesMs;
+            byte channelBitmask;
+            byte numSamples;
+        }
+
+        private SerialPort serialPort;
+        private ReceivedCommStates receivedCommState = ReceivedCommStates.Idle;
+        private byte lastReceivedByte = 0;
+        private int receivedLength;
+        private byte receivedChecksum;
+        private ReceivedPacket receivedPacket;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeSerialPorts();
+            receivedPacket.payload = new byte[MAX_PACKET_PAYLOAD_LEN];
+        }
+
+        private void LogToFile(string filename, string output)
+        {
+            try
+            {
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(filename, true))
+                {
+                    file.WriteLine(output);
+                }
+            }
+            catch
+            {
+                // Do nothing
+            }
+        }
+
+        private void HandlePacket(ReceivedPacket packet)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new MethodInvoker(() => HandlePacket(packet)));
+                return;
+            }
+
+            string outputFilename = OutputFileTextBox.Text;
+
+            switch (packet.command)
+            {
+                case 0x00:
+                    LogToFile(outputFilename, "Packet type 0: " + packet.payload[0].ToString() + " " + packet.payload[1].ToString());
+                    break;
+
+                case 0x10:
+                    // Parse read data packet
+                    break;
+            }
+        }
+
+        private void SerialDataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            const int MAX_RECEIVE_SIZE = MAX_PACKET_PAYLOAD_LEN + 20;
+            SerialPort sp = (SerialPort)sender;
+            byte[] readbytes = new byte[MAX_RECEIVE_SIZE];
+            int receivedBytes = sp.Read(readbytes, 0, MAX_RECEIVE_SIZE);
+            int i;
+            for (i = 0; i < receivedBytes; i++)
+            {
+                ProcessReceivedByte(readbytes[i]);
+            }
+        }
+
+        private void ProcessReceivedByte(byte c)
+        {
+            switch (receivedCommState)
+            {
+                case ReceivedCommStates.Idle:
+                    if (lastReceivedByte == START_BYTE)
+                    {
+                        receivedPacket.protocol = c;
+                        receivedChecksum = c;
+                        if (receivedPacket.protocol == PROTOCOL_VERSION_0)
+                        {
+                            receivedCommState = ReceivedCommStates.ReceiveLength;
+                        }
+                        else
+                        {
+                            receivedCommState = ReceivedCommStates.Idle;
+                        }
+                    }
+                    break;
+
+                case ReceivedCommStates.ReceiveLength:
+                    receivedPacket.length = c;
+                    receivedChecksum += c;
+                    receivedCommState = ReceivedCommStates.ReceiveCommand;
+                    break;
+
+                case ReceivedCommStates.ReceiveCommand:
+                    receivedPacket.command = c;
+                    receivedChecksum += c;
+                    receivedLength = 0;
+                    receivedCommState = ReceivedCommStates.ReceiveData;
+                    break;
+
+                case ReceivedCommStates.ReceiveData:
+                    if (receivedLength < receivedPacket.length)
+                    {
+                        receivedPacket.payload[receivedLength] = c;
+                        receivedChecksum += c;
+                        receivedLength++;
+                    }
+                    if (receivedLength >= receivedPacket.length)
+                    {
+                        receivedCommState = ReceivedCommStates.ReceiveChecksum;
+                    }
+                    break;
+
+                case ReceivedCommStates.ReceiveChecksum:
+                    if (c == receivedChecksum)
+                    {
+                        // Completed packet
+                        HandlePacket(receivedPacket);
+                    }
+                    receivedCommState = ReceivedCommStates.Idle;
+                    break;
+
+            }
+            lastReceivedByte = c;
+        }
+
+        public void InitializeSerialPorts()
+        {
+            string[] serialPorts = SerialPort.GetPortNames();
+            foreach (string serialPort in serialPorts)
+            {
+                serialPortList.Items.Add(serialPort);
+            }
+            if (serialPortList.Items.Count > 0)
+            {
+                serialPortList.SelectedItem = serialPortList.Items[0];
+            }
+        }
+
+        private void OpenCloseButton_Click(object sender, EventArgs e)
+        {
+            if (serialPort != null && serialPort.IsOpen )
+            {
+                // Close the currently open serial port
+                serialPort.Close();
+            }
+            OpenCloseButton.Text = "Open";
+            string selectedSerialPort = serialPortList.SelectedItem.ToString();
+            if (selectedSerialPort.Length > 0)
+            {
+                // Try to open the serial port
+                serialPort = new SerialPort(selectedSerialPort);
+                serialPort.BaudRate = 115200;
+                serialPort.DataBits = 8;
+                serialPort.Parity = Parity.None;
+                serialPort.StopBits = StopBits.One;
+                serialPort.Handshake = Handshake.None;
+
+                serialPort.DataReceived += new SerialDataReceivedEventHandler(SerialDataReceivedHandler);
+                serialPort.Open();
+                if (serialPort.IsOpen)
+                {
+                    OpenCloseButton.Text = "Close";
+                }
+            }
+        }
+    }
+}
